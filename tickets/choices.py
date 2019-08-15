74dba85a1a97a3b9c1634ec42724f01e8e55fcab
Hamidreza <hamidranjbarpour771@gmail.com>
2019-08-15 12:56:55 +0430

    add a choicefield to ticket objects

---
+++ tickets/choices.py
@@ -0,0 +1,9 @@
+Immediate = 1
+Semi_Immediate = 2
+Not_Immediate = 3
+
+PRIORITY_CHOICES = (
+        (Immediate, 'Immediate'),
+        (Semi_Immediate, 'Semi-Immediate'),
+        (Not_Immediate, 'Not-Immediate'),
+)
